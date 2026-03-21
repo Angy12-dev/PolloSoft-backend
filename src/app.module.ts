f0b2e79168524852b726a239d585576cba61bba9
Angy <angyzpao@gmail.com>
2026-03-21 12:07:28 -0500

    Agregar módulos para la gestión de usuarios, ventas, inventario, lotes, rentabilidad y costos de producción

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,13 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import typeorm from './config/typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersEntity } from './entities/users.entity';
+import { usersModule } from './users/users.module';
+import { salesModule } from './sales/sales.module';
+import { inventoryModule } from './inventory/inventory.module';
+import { batchModule } from './batch/batch.module';
+import { profitabilityModule } from './profitability/profitability.module';
+import { costProductionModule } from './costsProduction/costsProduction.module';
 
 @Module({
     imports: [
@@ -15,6 +22,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
             inject: [ConfigService],
             useFactory: (config: ConfigService) => config.get('typeorm') ?? {},
         }),
+        TypeOrmModule.forFeature([UsersEntity]),
+        usersModule,
+        salesModule,
+        inventoryModule,
+        batchModule,
+        profitabilityModule,
+        costProductionModule,
     ],
     controllers: [AppController],
     providers: [AppService],
