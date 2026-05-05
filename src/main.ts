7af625e52cce8de264b535d43361b5c83a6fbf44
Angy <angyzpao@gmail.com>
2026-05-05 12:49:22 -0500

    Actualizar configuración de TypeORM para usar DATABASE_URL y habilitar CORS en el servidor

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { loggerGlobal } from './middleware/loggerGlobal';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.use(loggerGlobal);
-    await app.listen(3002);
-    console.log('Servidor corriendo en el puerto 3002');
+    app.enableCors();
+    await app.listen(process.env.PORT || 3000);
+    console.log(`Servidor corriendo en el puerto ${process.env.PORT || 3000}`);
 }
 bootstrap();
